212de8f06916cd05eb135601e03bfa6939ff045e
Yago <yagomahler@gmail.com>
2025-11-08 16:16:04 -0300

    Router adicionado

--- Projeto Final React Grupo 5/src/routes/AppRouter.jsx
+++ Projeto Final React Grupo 5/src/routes/AppRouter.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Main from "../screens/Main";
+import Login from '../screens/Login';
+//import Produto from '../screens/Produto';
+import Categoria from '../screens/Categoria';
+import Compras from '../screens/Compras';
+import Sobre from '../screens/Sobre';
+
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+    <Routes>
+      <Route path="/" element={<Main/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
+      {/*<Route path="/produto/:id" element={<Produto/>}></Route>*/}
+      <Route path="/categoria" element={<Categoria/>}></Route> {/*categoria/:id*/}
+      <Route path="/compras" element={<Compras/>}></Route>
+      <Route path="/sobre" element={<Sobre/>}></Route>
+    </Routes>
   )
 }
 
